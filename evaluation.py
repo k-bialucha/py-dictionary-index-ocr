130a6c8f8a7d25b359655bd5efff2570731d01d2
Kamil Bialucha <kamil.bialucha@gmail.com>
2020-06-03 09:38:02 +0200

    Add reading reference data
    * implement CSV file reading
    * create initial comparing function
    * create initial reference data file

--- evaluation.py
+++ evaluation.py
@@ -1,6 +1,8 @@
 '''
 Main module for app's performance evaluation.
 '''
+import pandas as pd
+
 from input import parse_arguments
 from processing import process_image, OutputFormat
 
@@ -16,11 +18,26 @@ def evaluate():
     debug = args['debug']
     language = args['lang']
 
+    base_name = path.basename(image_path).split('.')[0]
+
     processing_result = process_image(
         image_path, preprocess_mode, language, debug, OutputFormat.data_frame_stripped)
 
     print("Result:")
     print(processing_result)
+
+    reference_data = pd.read_csv("./reference_data/{}.csv".format(base_name))
+
+    compare(processing_result, reference_data)
+
+
+def compare(actual_data, reference_data):
+    '''
+    Performs comparison between actual and reference data.
+    '''
+    print('comapre')
+    print(actual_data)
+    print(reference_data)
 
 
 if __name__ == "__main__":
