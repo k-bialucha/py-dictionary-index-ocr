ce8791447d85f9ab15e71c9a44855f7390f05369
Kamil Bialucha <kamil.bialucha@gmail.com>
2020-06-04 23:04:33 +0200

    Handle checking matches for actual data in reference data

--- evaluation.py
+++ evaluation.py
@@ -24,13 +24,47 @@ def evaluate():
     compare(processing_result, reference_data)
 
 
+empty_df = pd.DataFrame(
+    columns=['left', 'top', 'width', 'height', 'conf', 'text'])
+
+
 def compare(actual_data, reference_data):
     '''
     Performs comparison between actual and reference data.
     '''
-    print('comapre')
-    print(actual_data)
-    print(reference_data)
+    true_positives = empty_df.copy()
+    false_negatives = empty_df.copy()
+    false_positives = empty_df.copy()
+
+    # check reference data for matches with actual data
+    # and false negatives
+    for _, row in reference_data.iterrows():
+        x_pos = row['left']
+        y_pos = row['top']
+
+        query = 'left > {} and left < {} and top > {} and top < {}'.format(
+            x_pos-7, x_pos+7, y_pos-12, y_pos+12)
+        matching = actual_data.query(query)
+
+        results_count = len(matching.index)
+
+        if results_count == 1:
+            true_positives = true_positives.append(row)
+        elif results_count == 0:
+            false_negatives = false_negatives.append(row)
+        else:
+            print('WRONG RESULTS COUNT:', results_count)
+            raise SystemExit('evaluation/compare: invalid result count')
+
+    # check actual data for matches with reference data
+    # and false positives
+    for index, row in actual_data.iterrows():
+        print(row['text'], row['left'], row['top'])
+
+    print('============\nRESULTS:')
+    print(true_positives)
+    print(false_negatives)
+    print(false_positives)
 
 
 if __name__ == "__main__":
