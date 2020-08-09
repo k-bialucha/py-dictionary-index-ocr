eacee3bc2061e4e820f4441349fd304bd5db9de2
Kamil Bialucha <kamil.bialucha@gmail.com>
2020-08-09 21:02:48 +0200

    Handle basic page index in main.py module

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import glob
 import itertools
 
 from input import parse_main_arguments
-from processing import process_image
+from processing import get_page_index, process_image
 
 
 def main():
@@ -25,9 +25,13 @@ def main():
     paths = list(itertools.chain.from_iterable(paths_globbed))
 
     for image_path in paths:
-        processing_result = process_image(image_path, preprocess_mode, language, debug, config_name)
-        print("==========\nResults:\n")
+        page_index = get_page_index(image_path)
+
+        processing_result = process_image(
+            image_path, preprocess_mode, language, debug, config_name)
+        print("==========\nResults p.{}:\n".format(page_index))
         print(processing_result)
+
 
 if __name__ == "__main__":
     main()
