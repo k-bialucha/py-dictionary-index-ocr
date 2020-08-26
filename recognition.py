0ec58543420cf94e0d11b09ca8a978defc41a1a7
Kamil Bialucha <kamil.bialucha@gmail.com>
2020-08-27 01:48:16 +0200

    Extract normalization functions to a separate module

--- recognition.py
+++ recognition.py
@@ -6,41 +6,7 @@ import pytesseract
 from PIL import Image
 import pandas as pd
 
-
-def normalize_word(word: str):
-    '''
-    Takes recognized word and cleans it.
-    Removes trailing comma sign if exists
-    and performs OCR mistakes normalization
-    '''
-    if not isinstance(word, str):
-        return word
-
-    normalized_word = word
-
-    if normalized_word[-1] == ',':
-        normalized_word = normalized_word[0:-1]
-
-    return normalize_ocr_mistakes(normalized_word)
-
-
-def normalize_ocr_mistakes(word: str):
-    '''
-    Takes recognized word and cleans it.
-    Replaces long "s" to short "s" (for Fraktur)
-    and replaces 'ift' with 'ist'
-    '''
-    if not isinstance(word, str):
-        return ''
-
-    if 'ift' in word:
-        return 'ist'
-
-    normalized_word = word
-
-    normalized_word = normalized_word.replace('ſ', 's')
-
-    return normalized_word
+from normalization import normalize_word, normalize_ocr_mistakes
 
 
 SECOND_PART_KEYWORDS = list(['ist', 'oder', 'nieder'])
